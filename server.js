ecbd6ca556ce15e51547649547f704ff9991614b
naadiaa <nadiamoustafa68@gmail.com>
2023-05-26 00:26:09 +0300

    dotenv

--- server.js
+++ server.js
@@ -3,12 +3,11 @@ import { engine } from 'express-handlebars';
  
 import mongoose from "mongoose";
 import dotenv from "dotenv";
-import subjectsRouter from "./routes/subjectsRouter.js";
-import departmentRouter from "./routes/departmentRouter.js";
+ 
 
 dotenv.config();
 
-const mongoconnectionUrl = "mongodb://127.0.0.1:27017/project";
+ 
 mongoose.connect(process.env.mongoconnectionUrl);
 
 const app = express();
@@ -20,8 +19,6 @@ app.set('view engine', 'handlebars');
 app.set('views', './Templates');
 
  
-app.use("/subjects",subjectsRouter);
-app.use("/departments",departmentRouter);
 
 app.listen(process.env.port,() =>{
 console.log("started the application on http://localhost:" + process.env.port );
